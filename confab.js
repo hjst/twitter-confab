386699b8d7bb9d4ea2fcb15c8c91d0a557394e44
Henry Todd <henry@hjst.org>
2012-07-21 14:11:40 +0800

    Added omit_script=1 to the oembed ajax call

--- confab.js
+++ confab.js
@@ -3,10 +3,11 @@ var loadConfabulatedTweets = function(tweet, tweetReqStatus) {
 
   // get the oembed details for the tweet
   $.ajax({
-    url:'http://api.twitter.com/1/statuses/oembed.json?id='+tweet.id_str,
+    url:'http://api.twitter.com/1/statuses/oembed.json?id='+tweet.id_str+'&omit_script=1',
     dataType: 'jsonp',
     error: function(){},
     success: function(embed, embedReqStatus) {
+      console.log(embed);
       $('#confabulation').after(embed.html);
       // if this tweet is in reply to another - act on that as well
       if (tweet.in_reply_to_status_id_str) {
