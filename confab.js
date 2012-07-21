39a9d775b807c88fe20ebf21f6ec59f6df822822
Henry Todd <henry@hjst.org>
2012-07-21 13:57:15 +0800

    First sort of working version
    Very basic display of the raw embed HTML. The ordering of results also
    needs work.

--- confab.js
+++ confab.js
@@ -1,8 +1,42 @@
+var loadConfabulatedTweets = function(tweet, tweetReqStatus) {
+  console.log(tweet.id_str, ': ', tweet.text);
+
+  // get the oembed details for the tweet
+  $.ajax({
+    url:'http://api.twitter.com/1/statuses/oembed.json?id='+tweet.id_str+'&omit_script=1',
+    dataType: 'jsonp',
+    error: function(){},
+    success: function(embed, embedReqStatus) {
+      $('#confabulation').after(embed.html);
+    }
+  });
+
+  // if this tweet is in reply to another - act on that as well
+  if (tweet.in_reply_to_status_id_str) {
+    $.ajax({
+      url:'http://api.twitter.com/1/statuses/show/'+tweet.in_reply_to_status_id_str+'.json?trim_user=1',
+      dataType: 'jsonp',
+      error: function(){},
+      success: loadConfabulatedTweets
+    });
+  }
+};
+
 // bind the tweet loader to the button when the document is complete
 $(document).ready( function() {
   $('#confabulation').submit(function() {
     var tweet_input = $(this).children('input#contweet').val();
-    alert(tweet_input);
+    var matches = tweet_input.match(/(\d+)/g);
+    // use the last number in the URL/input, which should be the id
+    var initial_tweet_id = matches[matches.length-1];
+
+    $.ajax({
+      url:'http://api.twitter.com/1/statuses/show/'+initial_tweet_id+'.json?trim_user=1',
+      dataType: 'jsonp',
+      error: function(){},
+      success: loadConfabulatedTweets
+    });
+
     return false;
-  })
+  });
 });
