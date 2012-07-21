2735d323e21d66e773e0238c4a4b35b34f62efbb
Henry Todd <henry@hjst.org>
2012-07-21 14:41:48 +0800

    First working version with web intents

--- confab.js
+++ confab.js
@@ -3,7 +3,9 @@ var loadConfabulatedTweets = function(tweet, tweetReqStatus) {
 
   // get the oembed details for the tweet
   $.ajax({
-    url:'http://api.twitter.com/1/statuses/oembed.json?id='+tweet.id_str+'&omit_script=1',
+    url:'http://api.twitter.com/1/statuses/oembed.json?'+
+      'id='+tweet.id_str+
+      '&hide_thread=1',
     dataType: 'jsonp',
     error: function(){},
     success: function(embed, embedReqStatus) {
@@ -12,7 +14,9 @@ var loadConfabulatedTweets = function(tweet, tweetReqStatus) {
       // if this tweet is in reply to another - act on that as well
       if (tweet.in_reply_to_status_id_str) {
         $.ajax({
-          url:'http://api.twitter.com/1/statuses/show/'+tweet.in_reply_to_status_id_str+'.json?trim_user=1',
+          url:'http://api.twitter.com/1/statuses/show/'+
+            tweet.in_reply_to_status_id_str+'.json?'+
+            'trim_user=1',
           dataType: 'jsonp',
           error: function(){},
           success: loadConfabulatedTweets
